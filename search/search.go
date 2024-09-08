fe907ed63c02322924dcf863f4574d889ed97c3d
Inex- <87673397+Inexistentex@users.noreply.github.com>
2024-09-07 22:29:01 -0300

    search.go

--- search/search.go
+++ search/search.go
@@ -4,16 +4,14 @@ import (
 	"encoding/json"
 	"fmt"
 	"math/big"
-	"math/rand"
 	"os"
 	"strings"
-	"sync" // Adicione esta linha
+	
 	"sync/atomic"
 	"time"
-
-	"btchunt/wif"
-
-	"github.com/dustin/go-humanize"
+	"math/rand"
+        "github.com/dustin/go-humanize"
+	"btchunt/wif" // Importando o pacote wif
 )
 
 // IntervalJumper estrutura para gerenciar o salto entre intervalos
@@ -86,73 +84,107 @@ type Ranges struct {
 	Ranges []Range
 }
 
-var once sync.Once
+// LoadRanges carrega os intervalos de chaves privadas a partir de um arquivo JSON e converte os endereços em hash160
+func LoadRanges(filename string) (*Ranges, error) {
+	file, err := os.Open(filename)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
 
+	var ranges Ranges
+	decoder := json.NewDecoder(file)
+	err = decoder.Decode(&ranges)
+	if err != nil {
+		return nil, err
+	}
 
-// SearchInBlock busca por chaves privadas em um bloco de tamanho blockSize dentro do intervalo fornecido
-func SearchInBlock(wallets []string, blockSize int64, minPrivKey, maxPrivKey *big.Int, stopSignal chan struct{}, startTime time.Time, id int, keysChecked *int64, checkInterval int64) {
-	for {
-		select {
-		case <-stopSignal:
-			return
-		default:
-			block := getRandomBlock(minPrivKey, maxPrivKey, blockSize)
-			privKey := new(big.Int)
+	// Convert addresses to hash160
+	for i := range ranges.Ranges {
+		addresses := strings.Split(ranges.Ranges[i].Status, ", ")
+		var hash160s []string
+		for _, address := range addresses {
+			hash160 := wif.AddressToHash160(address) // Usando a função do pacote wif
+			hash160s = append(hash160s, fmt.Sprintf("%x", hash160))
+		}
+		ranges.Ranges[i].Status = strings.Join(hash160s, ", ")
+	}
 
-			for i := int64(0); i < blockSize; i++ {
-				select {
-				case <-stopSignal:
-					return
-				default:
-					privKey.Add(block, big.NewInt(i))
-					privKeyBytes := privKey.FillBytes(make([]byte, 32))
-					pubKey := wif.GeneratePublicKey(privKeyBytes)
-					addressHash160 := wif.Hash160(pubKey)
+	return &ranges, nil
+}
 
-					addressHash160Hex := fmt.Sprintf("%x", addressHash160)
-					if contains(wallets, addressHash160Hex) {
-						wifKey := wif.PrivateKeyToWIF(privKey)
-						address := wif.PublicKeyToAddress(pubKey)
-						saveFoundKeyDetails(privKey, wifKey, address)
+// GetRandomBlock gera um bloco de chaves privadas aleatórias dentro do intervalo fornecido
+func GetRandomBlock(minPrivKey, maxPrivKey *big.Int, blockSize int64) *big.Int {
+	rangeSize := new(big.Int).Sub(maxPrivKey, minPrivKey)
+	block := new(big.Int).Rand(rand.New(rand.NewSource(time.Now().UnixNano())), rangeSize)
 
-						// Garante que o canal seja fechado apenas uma vez
-						once.Do(func() {
-							close(stopSignal)
-						})
+	// Ajusta o bloco para estar dentro do intervalo [minPrivKey, maxPrivKey - blockSize]
+	block.Add(block, minPrivKey)
+	blockEnd := new(big.Int).Set(block)
+	blockEnd.Add(blockEnd, big.NewInt(blockSize))
 
-						return
-					}
+	if blockEnd.Cmp(maxPrivKey) > 0 {
+		// Se o bloco ultrapassar o maxPrivKey, ajusta para estar dentro do intervalo
+		block.Sub(maxPrivKey, big.NewInt(blockSize))
+	}
 
-					if atomic.AddInt64(keysChecked, 1)%checkInterval == 0 {
-						printProgress(startTime, keysChecked)
-					}
-				}
+	return block
+}
+
+// SearchInBlockBatch busca chaves privadas em lotes dentro de um bloco
+func SearchInBlockBatch(wallets []string, blockSize int64, minPrivKey, maxPrivKey *big.Int, stopSignal chan struct{}, startTime time.Time, id int, keysChecked *int64, checkInterval int64, taskChan chan *big.Int, batchSize int) {
+	for block := range taskChan {
+		privKey := new(big.Int)
+		var privKeyBatch []*big.Int
+		for i := int64(0); i < blockSize; i++ {
+			privKey.Add(block, big.NewInt(i))
+			privKeyBatch = append(privKeyBatch, new(big.Int).Set(privKey))
+
+			// Processa o lote ao atingir o tamanho do batch
+			if len(privKeyBatch) == batchSize {
+				verifyBatch(privKeyBatch, wallets, stopSignal, keysChecked, checkInterval, startTime)
+				privKeyBatch = privKeyBatch[:0] // Limpa o batch
 			}
+		}
+		// Processa o restante do lote
+		if len(privKeyBatch) > 0 {
+			verifyBatch(privKeyBatch, wallets, stopSignal, keysChecked, checkInterval, startTime)
 		}
 	}
 }
-// getRandomBlock gera um bloco de chaves privadas aleatórias dentro do intervalo fornecido
-func getRandomBlock(minPrivKey, maxPrivKey *big.Int, blockSize int64) *big.Int {
-    rangeSize := new(big.Int).Sub(maxPrivKey, minPrivKey)
-    block := new(big.Int).Rand(rand.New(rand.NewSource(time.Now().UnixNano())), rangeSize)
 
-    // Ajusta o bloco para estar dentro do intervalo [minPrivKey, maxPrivKey - blockSize]
-    block.Add(block, minPrivKey)
-    blockEnd := new(big.Int).Set(block)
-    blockEnd.Add(blockEnd, big.NewInt(blockSize))
+// verifyBatch verifica um lote de chaves de uma só vez
+func verifyBatch(privKeyBatch []*big.Int, wallets []string, stopSignal chan struct{}, keysChecked *int64, checkInterval int64, startTime time.Time) {
+	pubKeys := make([][]byte, len(privKeyBatch))
+	for i, privKey := range privKeyBatch {
+		privKeyBytes := privKey.FillBytes(make([]byte, 32))
+		pubKeys[i] = wif.GeneratePublicKey(privKeyBytes) // Usando função do pacote wif
+	}
 
-    if blockEnd.Cmp(maxPrivKey) > 0 {
-        // Se o bloco ultrapassar o maxPrivKey, ajusta para estar dentro do intervalo
-        block.Sub(maxPrivKey, big.NewInt(blockSize))
-    }
+	for i, pubKey := range pubKeys {
+		addressHash160 := wif.Hash160(pubKey) // Usando função do pacote wif
+		addressHash160Hex := fmt.Sprintf("%x", addressHash160)
 
-    return block
+		if contains(wallets, addressHash160Hex) {
+			privKey := privKeyBatch[i]
+			wifKey := wif.PrivateKeyToWIF(privKey) // Usando função do pacote wif
+			address := wif.PublicKeyToAddress(pubKey) // Usando função do pacote wif
+			saveFoundKeyDetails(privKey, wifKey, address)
+
+			close(stopSignal)
+			return
+		}
+	}
+
+	if atomic.AddInt64(keysChecked, int64(len(privKeyBatch)))%checkInterval == 0 {
+		printProgress(startTime, keysChecked)
+	}
 }
 
-// contains verifica se um item está presente em um slice
-func contains(slice []string, item string) bool {
-	for _, s := range slice {
-		if s == item {
+// contains verifica se um endereço hash está na lista de wallets
+func contains(wallets []string, addressHash160Hex string) bool {
+	for _, wallet := range wallets {
+		if wallet == addressHash160Hex {
 			return true
 		}
 	}
@@ -179,34 +211,6 @@ func saveFoundKeyDetails(privKey *big.Int, wifKey, address string) {
 	}
 }
 
-// LoadRanges carrega os intervalos de chaves privadas a partir de um arquivo JSON e converte os endereços em hash160
-func LoadRanges(filename string) (*Ranges, error) {
-	file, err := os.Open(filename)
-	if err != nil {
-		return nil, err
-	}
-	defer file.Close()
-
-	var ranges Ranges
-	decoder := json.NewDecoder(file)
-	err = decoder.Decode(&ranges)
-	if err != nil {
-		return nil, err
-	}
-
-	// Convert addresses to hash160
-	for i := range ranges.Ranges {
-		addresses := strings.Split(ranges.Ranges[i].Status, ", ")
-		var hash160s []string
-		for _, address := range addresses {
-			hash160 := wif.AddressToHash160(address)
-			hash160s = append(hash160s, fmt.Sprintf("%x", hash160))
-		}
-		ranges.Ranges[i].Status = strings.Join(hash160s, ", ")
-	}
-
-	return &ranges, nil
-}
 
 // printProgress imprime o progresso da busca
 func printProgress(startTime time.Time, keysChecked *int64) {
