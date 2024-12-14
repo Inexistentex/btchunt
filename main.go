5a55e39c77b88870b0974aa9ba93dad538d85b7c
Inex- <87673397+Inexistentex@users.noreply.github.com>
2024-12-14 16:38:47 -0300

    Update main.go

--- main.go
+++ main.go
@@ -103,7 +103,7 @@ func main() {
 			wg.Wait()
 			return
 		default:
-			block := search.GetRandomBlock(privKeyInt, maxPrivKeyInt, blockSize)
+			block := search.GetRandomBlock(privKeyInt, maxPrivKeyInt, blockSize, false)
 			taskChan <- block
 		}
 	}
